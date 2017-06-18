47314be3ec3958fa5a78b923b22ea7a563050c4a
wishinghand <wishinghand@gmail.com>
2017-06-17 19:20:55 -0700

    new code of conduct

--- pages/codeofconduct.vue
+++ pages/codeofconduct.vue
@@ -2,42 +2,66 @@
 .container
   .container__inner
     :markdown-it
-      # Conference Code of Conduct
+      # Code of Conduct
 
-      All attendees, speakers, sponsors and volunteers at our conference are required to agree with the following code of conduct. Organisers will enforce this code throughout the event. We expect cooperation from all participants to help ensure a safe environment for everybody.
+      San Diego JS is dedicated to providing a welcoming and inclusive experience for everyone. We do not tolerate unacceptable behavior of participants in any form.
 
-      ## Need Help?
+      This code of conduct applies to all San Diego JS spaces, both online and off, including our mailing lists, meetup group, and any location where we are meeting in person. Anyone who violates this code of conduct may be sanctioned or expelled from these spaces at the discretion of the San Diego JS organizers.
 
-      Emergency contact email and phone number will be added here before the event.
+      Some San Diego JS spaces may have additional rules in place, which will be made clearly available to participants. Participants are responsible for knowing and abiding by these rules.
 
-      ## The Quick Version
+      # Recommended Conduct
 
-      Our conference is dedicated to providing a harassment-free conference experience for everyone, regardless of gender, gender identity and expression, age, sexual orientation, disability, physical appearance, body size, race, ethnicity, religion (or lack thereof), or technology choices. We do not tolerate harassment of conference participants in any form. Sexual language and imagery is not appropriate for any conference venue, including talks, workshops, parties, Twitter and other online media. Conference participants violating these rules may be sanctioned or expelled from the conference without a refund at the discretion of the conference organisers.
+      - **Be welcoming, friendly, and patient.**
+      - **Be respectful.** Not all of us will agree all the time, but disagreement is no excuse for poor behavior and poor manners. We might all experience some frustration now and then, but we cannot allow that frustration to turn into a personal attack. It’s important to remember that a community where people feel uncomfortable or threatened is not a productive one. Members of the San Diego JS community should be respectful when dealing with other members as well as with people outside the San Diego JS community.
+      - **When we disagree, try to understand why.** Disagreements, both social and technical, happen all the time and San Diego JS is no exception. It is important that we resolve disagreements and differing views constructively. Remember that we’re different. The strength of San Diego JS comes from its varied community, people from a wide range of backgrounds. Different people have different perspectives on issues. Being unable to understand why someone holds a viewpoint doesn’t mean that they’re wrong. Don’t forget that it is human to err and blaming each other doesn’t get us anywhere, rather offer to help resolving issues and to help learn from mistakes.
 
-      ## The Less Quick Version
+      # Unacceptable Behavior
 
-      Harassment includes offensive verbal comments related to gender, gender identity and expression, age, sexual orientation, disability, physical appearance, body size, race, ethnicity, religion, technology choices, sexual images in public spaces, deliberate intimidation, stalking, following, harassing photography or recording, sustained disruption of talks or other events, inappropriate physical contact, and unwelcome sexual attention.
+      - Offensive comments related to gender, gender identity and expression, sexual orientation, disability, mental illness, neuro(a)typicality, physical appearance, body size, race, or religion
+      - Unwelcome comments regarding a person's lifestyle choices and practices, including those related to food, health, parenting, drugs, and employment.
+      - Gratuitous or off-topic sexual images or behavior  in spaces where they're not appropriate
+      - Physical contact and simulated physical contact (eg, textual descriptions like "*hug*" or "*backrub*") without consent or after a request to stop.
+      - Threats of violence
+      - Incitement of violence towards any individual, including encouraging a person to commit suicide or to engage in self-harm
+      - Deliberate intimidation
+      - Stalking or following
+      - Harassing photography or recording, including logging online activity for harassment purposes
+      - Unwelcome sexual attention
+      - Pattern of inappropriate social contact, such as requesting/assuming inappropriate levels of intimacy with others
+      - Continued one-on-one communication after requests to cease
+      - Deliberate "outing" of any aspect of a person's identity without their consent except as necessary to protect vulnerable people from intentional abuse
+      - Publication of non-harassing private communication
 
-      Participants asked to stop any harassing behavior are expected to comply immediately.
+      **San Diego JS prioritizes marginalized people's safety over privileged people's comfort. San Diego JS organizers will not act on complaints regarding:**
 
-      Sponsors are also subject to the anti-harassment policy. In particular, sponsors should not use sexualised images, activities, or other material. Booth staff (including volunteers) should not use sexualised clothing/uniforms/costumes, or otherwise create a sexualised environment.
+      - 'Reverse' -isms, including 'reverse racism,' 'reverse sexism,' and 'cisphobia'
+      - Reasonable communication of boundaries, such as "leave me alone," "go away," or "I'm not discussing this with you."
+      - Communicating in a 'tone' you don't find congenial
+      - Criticizing racist, sexist, cissexist, or otherwise oppressive behavior or assumptions
 
-      If a participant engages in harassing behavior, the conference organisers may take any action they deem appropriate, including warning the offender or expulsion from the conference with no refund.
+      # Reporting
 
-      If you are being harassed, notice that someone else is being harassed, or have any other concerns, please contact a member of conference staff immediately. Conference staff can be identified as they'll be wearing branded clothing and/or badges.
+      If you are being harassed by a member of San Diego JS, notice that someone else is being harassed, or have any other concerns, please contact the San Diego JS organizers at sandiegojs-organizers (at) googlegroups (dot) com. If the person who is harassing you is on the team, they will recuse themselves from handling your incident. We will respond as promptly as we can.
 
-      Conference staff will be happy to help participants contact hotel/venue security or local law enforcement, provide escorts, or otherwise assist those experiencing harassment to feel safe for the duration of the conference. We value your attendance.
+      This code of conduct applies to San Diego JS spaces, but if you are being harassed by a member of San Diego JS outside our spaces, we still want to know about it. We will take all good-faith reports of unacceptable behavior by San Diego JS members, especially San Diego JS organizers, seriously. This includes unacceptable behavior outside our spaces and unacceptable behavior that took place at any point in time. The abuse team reserves the right to exclude people from San Diego JS based on their past behavior, including behavior outside San Diego JS spaces and behavior towards people who are not in San Diego JS.
 
-      We expect participants to follow these rules at conference and workshop venues and conference-related social events.
+      In order to protect volunteers from abuse and burnout, we reserve the right to reject any report we believe to have been made in bad faith. Reports intended to silence legitimate criticism may be deleted without response.
 
-      Original source and credit: [http://2012.jsconf.us/#/about](http://2012.jsconf.us/#/about) & The Ada Initiative
+      We will respect confidentiality requests for the purpose of protecting victims of abuse. At our discretion, we may publicly name a person about whom we've received harassment complaints, or privately warn third parties about them, if we believe that doing so will increase the safety of San Diego JS members or the general public. We will not name harassment victims without their affirmative consent.
 
-      This work is licensed under a Creative Commons Attribution 3.0 Unported License
+      # Consequences
+
+      Participants asked to stop any unacceptable behavior are expected to comply immediately.
+
+      If a participant engages in unacceptable behavior, San Diego JS organizers may take any action they deem appropriate, up to and including expulsion from all San Diego JS spaces and identification of the participant as a harasser to other San Diego JS members or the general public.
+
+      Text adapted from [Geek Feminism](https://geekfeminism.org/about/code-of-conduct) and the [Django project](https://www.djangoproject.com/conduct/).
 </template>
 <script>
 export default {
   head: {
-    title: 'VueConf 2017 | Code of Conduct'
+    title: 'SDJS Hackathon 2017 | Code of Conduct'
   }
 }
 </script>
